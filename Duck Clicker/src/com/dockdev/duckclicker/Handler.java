bc781d538b1d203a28a696bcb8f42a2311ce08f7
Zaiden <zaiden.kennedy@gmail.com>
2016-10-15 16:33:54 +1100

    Added Items

--- Duck Clicker/src/com/dockdev/duckclicker/Handler.java
+++ Duck Clicker/src/com/dockdev/duckclicker/Handler.java
@@ -4,10 +4,16 @@ import java.util.LinkedList;
 	
 public class Handler {
 	LinkedList<GameObject> object = new LinkedList<GameObject>();
+	LinkedList<Item> item = new LinkedList<Item>();
+	
 	public void tick() {
 		for (int i = 0; i < object.size(); i++) {
 			GameObject tempObject = object.get(i);
 			tempObject.tick();
+		}
+		for (int i = 0; i < item.size(); i++){
+			Item tempItem = item.get(i);
+			tempItem.tick();
 		}
 	}
 	public void render(Graphics g) {
@@ -15,11 +21,22 @@ public class Handler {
 			GameObject tempObject = object.get(i);
 			tempObject.render(g);
 		}
+		for (int i = 0; i < item.size(); i++){
+			Item tempItem = item.get(i);
+			tempItem.render(g);
+			tempItem.iconRender(g);
+		}
 	}
 	public void addObject(GameObject object) {
 		this.object.add(object);
 	}
 	public void removeObject(GameObject object) {
 		this.object.remove(object);
+	}
+	public void addItem(Item item) {
+		this.item.add(item);
+	}
+	public void removeItem(Item item) {
+		this.item.remove(item);
 	}
 }
---
+++ Duck Clicker/src/com/dockdev/duckclicker/Item.java
@@ -0,0 +1,47 @@
+package com.dockdev.duckclicker;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+public abstract class Item {
+	
+	protected int price, dps, pval;
+	protected String image;
+	
+	BufferedImage icon;
+	
+	public Item(int price, int dps, int pval, String image) {
+		this.price = price;
+		this.dps = dps;
+		this.image = image;
+		this.pval = pval;
+		
+		try {
+			icon = ImageIO.read(getClass().getResourceAsStream(image));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public abstract void tick();
+	public abstract void render(Graphics g);
+	
+	public void iconRender(Graphics g){
+		g.drawImage(icon, 0, 0, null);
+	}
+	
+	public int getPrice(){
+		return price;
+	}
+	
+	public int getDps(){
+		return dps;
+	}
+	
+	public int getPval(){
+		return pval;
+	}
+	
+}
