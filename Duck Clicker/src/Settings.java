ea61481b686c6e60439006a098e6a19da00c40de
Uno <unogib@gmail.com>
2016-10-15 12:26:39 +1100

    Added Settings Icon

--- Duck Clicker/src/Settings.java
+++ Duck Clicker/src/Settings.java
@@ -1,4 +1,13 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 public class Settings {
-
+	
+	BufferedImage settings;
+	
+	public void render(Graphics g){
+		
+		g.drawImage(settings,  WIDTH, HEIGHT, null);
+		
+	}
 }
