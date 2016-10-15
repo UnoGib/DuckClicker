f5baa1d21361d929cc813d9ebe4207c69f980cce
Zaiden <zaiden.kennedy@gmail.com>
2016-10-15 12:44:55 +1100

    Added Market

--- Duck Clicker/src/Market.java
+++ Duck Clicker/src/Market.java
@@ -1,9 +1,22 @@
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 public class Market {
 	
+	BufferedImage market;
+	
+	public Market() {
+		try {
+			market = ImageIO.read(getClass().getResourceAsStream("/markethundp.png"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public void render(Graphics g){
-		
+		g.drawImage(market, 500, 340, null);
 	}
 	
 	public void tick(){
