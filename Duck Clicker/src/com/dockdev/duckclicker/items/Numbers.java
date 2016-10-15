f7a7c284db985cdbd1de4e0da7f2b52690621fa8
Uno <unogib@gmail.com>
2016-10-15 20:06:08 +1100

    Added /Numbers for quick Fractions and DPS and Cost

---
+++ Duck Clicker/src/com/dockdev/duckclicker/items/Numbers.java
@@ -0,0 +1,22 @@
+package com.dockdev.duckclicker.items;
+
+public class Numbers {
+	
+	//Fractions
+	public double third = 1 / 3;
+	public double third2 = third * 2;
+	public float quarter = 1 / 4;
+	public float half = 1 / 2;
+	public float eighth = 1 / 8;
+	public float sixtyfourth = 1 / 64;
+	public int whole = 1;
+	
+	//Item DPS and Cost
+	public int ponddps = 5;
+	public int pondcost = 100;
+	public int purplizerdps = 20;
+	public int purplizercost = 500;
+	public int duckdealerdps = 50;
+	public int duckdealercost = 2500;
+	
+}
