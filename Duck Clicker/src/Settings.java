60f99a2eea25c3ba9e0fab750b33a59b1620fffc
Uno <unogib@gmail.com>
2016-10-15 12:37:31 +1100

    Again, asi

--- Duck Clicker/src/Settings.java
+++ Duck Clicker/src/Settings.java
@@ -1,13 +1,22 @@
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
+
 public class Settings {
-	
 	BufferedImage settings;
+	public Settings() {
+		try {
+			settings = ImageIO.read(getClass().getResourceAsStream("/settingshundp.png"));
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
 	
 	public void render(Graphics g){
 		
-		g.drawImage(settings,  1, 2, null);
+		g.drawImage(settings,  350, 340, null);
 		
 	}
 }
