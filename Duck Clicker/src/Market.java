2f575044ba45aceebc4f84f21313e460bb11ece9
Uno <unogib@gmail.com>
2016-10-15 12:46:12 +1100

    rgqg

--- Duck Clicker/src/Market.java
+++ Duck Clicker/src/Market.java
@@ -1,7 +1,22 @@
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 public class Market {
+	BufferedImage market;
+	public Market() {
+		try {
+			market = ImageIO.read(getClass().getResourceAsStream("/markethundp.png"));
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	
 	public void render(Graphics g){
+		
+		g.drawImage(market,  100, 340, null);
 		
 	}
 }
